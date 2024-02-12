b76a8c2ef6fe60fe97d66ab51ecce09ffac0ff01
KarloBrsak <146105129+KarloBrsak@users.noreply.github.com>
2024-02-12 18:09:31 +0100

    Update app.js

--- Homework 2/app.js
+++ Homework 2/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 const connectToDatabase = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://kbrsak:apiY4NpHZtVxMnPI@cluster0.pzncpzh.mongodb.net/Homework-2?retryWrites=true&w=majority'
+      'mongodb+srv://kbrsak:<ADD PASSWORD>@cluster0.pzncpzh.mongodb.net/Homework-2?retryWrites=true&w=majority'
     );
   } catch (error) {
     console.log(error);
